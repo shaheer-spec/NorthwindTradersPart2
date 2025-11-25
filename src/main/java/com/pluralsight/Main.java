9978af82b018e49dc97cc9fb485d088f936992f7
Shaheer kayani <shaheer.kayani8@gmail.com>
2025-11-25 11:14:23 -0500

    Final code

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -2,15 +2,56 @@ package com.pluralsight;
 
 import java.sql.*;
 
-public class NorthwindTraders {
+public class Main {
     public static void main(String[] args) {
 
         try {
-            
+            if (args.length != 2){
+                System.out.println("There is not username and password.");
+                System.exit(1);
+            }
+
+            String username = args[0];
+            String password = args[1];
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection c = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/northwind",
+                    username,
+                    password
+            );
+
+            Statement statement = c.createStatement();
+
+            String query = """
+                    SELECT ProductID, ProductName, UnitPrice, UnitsInStock
+                    FROM northwind.products;
+                    """;
+
+            ResultSet results = statement.executeQuery(query);
+
+            while (results.next()){
+                int productID = results.getInt("ProductID");
+                String productName = results.getString("ProductName");
+                double unitPrice = results.getDouble("UnitPrice");
+                int unitsInStock = results.getInt("UnitsInStock");
+
+                System.out.println("Product ID: " + productID);
+                System.out.println("Name:       " + productName);
+                System.out.println("Price:      " + unitPrice);
+                System.out.println("Stock:      " + unitsInStock);
+                System.out.println("------------------------------------------------");
+            }
+
+            results.close();
+            statement.close();
+            c.close();
 
 
         } catch (Exception ex){
-
+            System.err.println("Exception found: " );
+            ex.printStackTrace();
         }
 
     }
