57331299493d3fc07b054eafb1680fa046e9e14d
Shaheer kayani <shaheer.kayani8@gmail.com>
2025-11-25 11:14:15 -0500

    Initial commit

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+import java.sql.*;
+
+public class NorthwindTraders {
+    public static void main(String[] args) {
+
+        try {
+            
+
+
+        } catch (Exception ex){
+
+        }
+
+    }
+}
